2449bd13d673e03f1e646c290ed02bb6c2dace7b
Diego Moreira <moreira.geo@gmail.com>
2020-04-12 02:23:26 -0300

    change time zone e language code

--- server/pangea/settings.py
+++ server/pangea/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.gis',
     'rest_framework', 
     'pangea_admin'
-
 ]
 
 MIDDLEWARE = [
@@ -139,9 +138,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Brazil/East'
 
 USE_I18N = True
 
