91a90e28e98a4005246a22683d4ac52c1298f514
Diego Moreira <moreira.geo@gmail.com>
2020-04-12 02:24:52 -0300

    refactoring urls

--- server/pangea_admin/urls.py
+++ server/pangea_admin/urls.py
@@ -8,14 +8,10 @@ router.register(r'users', views.UserViewSet)
 router.register(r'groups', views.GroupViewSet)
 router.register(r'permissions', views.PermissionViewSet)
 
-router.register(r'importedtabulardata', views.ImportedTabularDataViewSet)
-router.register(r'importedgeographicdata', views.ImportedGeographicDataViewSet)
+router.register(r'columns', views.ColumnViewSet)
+router.register(r'layerstatus', views.LayerStatusViewSet)
 
-router.register(r'datastatus', views.DataStatusViewSet)
-router.register(r'generalizationparams', views.GeneralizationParamsViewSet)
-router.register(r'layermetadata', views.LayerMetadataViewSet)
-router.register(r'column', views.ColumnViewSet)
-
+router.register(r'basicterritoriallevel', views.BasicTerritorialLevelLayerViewSet)
 
 
 # Wire up our API using automatic URL routing.
@@ -25,15 +21,5 @@ urlpatterns = [
     path(r'api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     path('tables/', views_aux._get_tables),
     path('geotables/', views_aux._get_geo_tables) ,
-    path('<table>/columns/', views_aux._get_colunms) 
+    path('<table>/columns/', views_aux._get_colunms),
 ] 
-
-
-
-'''
-    path(r'^getTables/$', 'spada.views.ajax_tabela_request'),
-    path(r'^getGeoTables/$', 'spada.views.ajax_geo_tabela_request'),
-    path(r'^getColumns/(?P<table>[\w\ ]*)/$', 'spada.views.ajax_coluna_request'),
-    path(r'^geocolunas/(?P<esquema>\w*)/(?P<tabela>[\w\ ]*)/$', 'spada.views.ajax_geo_coluna_request'),
-    path(r'^colunas_id/(?P<identificador>\d+)/$', 'spada.views.ajax_coluna_id_request'),
-'''
