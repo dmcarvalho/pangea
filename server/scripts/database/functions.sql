33e0735abc47f0e15678d2e50afb98f8279186af
Diego Moreira <moreira.geo@gmail.com>
2020-04-22 04:26:44 -0300

    postgre function - tilebox

---
+++ server/scripts/database/functions.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE FUNCTION public.tilebbox(z integer, x integer, y integer, srid integer DEFAULT 3857)
+ RETURNS geometry
+ LANGUAGE plpgsql
+ IMMUTABLE
+AS $function$
+declare
+    max numeric := 20037508.34;
+    res numeric := (max*2)/(2^z);
+    bbox geometry;
+begin
+    bbox := ST_MakeEnvelope(
+        -max + (x * res),
+        max - (y * res),
+        -max + (x * res) + res,
+        max - (y * res) - res,
+        3857
+    );
+    if srid = 3857 then
+        return bbox;
+    else
+        return ST_Transform(bbox, srid);
+    end if;
+end;
+$function$;
