aa7b893fc3598f027315a0679e2c2f01d75baf77
Diego Moreira <moreira.geo@gmail.com>
2020-04-25 00:09:15 -0300

    update scripts backup restore

--- server/scripts/database/restore-db.sh
+++ server/scripts/database/restore-db.sh
@@ -4,5 +4,17 @@ COMPOSE_FILE="docker-compose-dev.yaml"
 source $ENV_FILE
 
 FILE_NAME=`ls -t1 data/develpment/db-backup |  head -n 1`
-echo "restoring last generated backup"
-docker-compose -f $COMPOSE_FILE exec db pg_restore --no-owner --clean -U $POSTGRES_USER -d $POSTGRES_DB /db-backup/$FILE_NAME
+echo "Stopping APP"
+docker-compose -f $COMPOSE_FILE stop django
+
+echo "Dropping Database"
+docker-compose -f $COMPOSE_FILE exec db dropdb -U $POSTGRES_USER $POSTGRES_DB
+
+echo "Creating Database"
+docker-compose -f $COMPOSE_FILE exec db createdb -U $POSTGRES_USER $POSTGRES_DB
+
+echo "Restoring last generated backup"
+docker-compose -f $COMPOSE_FILE exec db pg_restore --no-owner -U $POSTGRES_USER -d $POSTGRES_DB /db-backup/$FILE_NAME
+
+echo "Restarting APP"
+docker-compose -f $COMPOSE_FILE start django
